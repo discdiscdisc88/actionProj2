2da32426a1a62923984b7530538e50dea85364be
discdiscdisc88 <84032051+discdiscdisc88@users.noreply.github.com>
2024-11-17 16:33:57 +0100

    Update InstallBrowser1.sh

--- scripts/InstallBrowser1.sh
+++ scripts/InstallBrowser1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 wget https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.106-1_amd64.deb
-sudo apt install ./google-chrome-stable_google-chrome-stable_114.0.5735.106-1_amd64.deb
+sudo apt install ./google-chrome-stable_114.0.5735.106-1_amd64.deb
