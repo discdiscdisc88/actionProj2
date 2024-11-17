ea4ec5e2d2cb7e0b69c21a004815bde458f9ffc9
discdiscdisc88 <84032051+discdiscdisc88@users.noreply.github.com>
2024-11-17 14:56:36 +0100

    Update2 Test1inTestNg1.java

--- src/test/java/myTest1/Test1inTestNg1.java
+++ src/test/java/myTest1/Test1inTestNg1.java
@@ -14,6 +14,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
